71f95415a81378e7647c83dc72ae971a9a65d1e0
kurnia-dev <kurniawanviic54@gmail.com>
2023-09-02 07:23:39 +0700

    modify HoveView to display Music Trends and Recomendation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="overflow-y-auto overflow-hidden">
+        <div class="py-8 px-12 flex flex-col gap-16">
+            <TrendingPlaylist />
+            <PlayListGroup groupTitle="Recommended" />
+            <PlayListGroup groupTitle="Recommended" />
+            <PlayListGroup groupTitle="Recommended" />
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PlayListGroup from '@/components/PlayListGroup.vue';
+import TrendingPlaylist from '@/components/TrendingPlaylist.vue';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    components: {
+        TrendingPlaylist,
+        PlayListGroup
+    }
 }
 </script>
