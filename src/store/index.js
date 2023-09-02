363b15fbfe6ae670d6eea7c9eaa3b0dc01f63cfc
kurnia-dev <kurniawanviic54@gmail.com>
2023-09-02 07:26:36 +0700

    add lastPlayed state and its mutations and actions

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    lastPlayed: {
+      thumb: "https://ik.imagekit.io/kurniadev/thumb.png?updatedAt=1693472807210",
+      title: "Back to Her Men", 
+      artist: "Demien Rice", 
+      isFavorite: false, 
+    }
   },
   mutations: {
+    lastPlayedUpdated(state, newSong) {
+      state.lastPlayed = newSong
+    }
   },
   actions: {
+    setLastPlayed({ commit }, newSong) {
+      commit("lastPlayedUpdated", newSong)
+    }
   },
   modules: {
   }
